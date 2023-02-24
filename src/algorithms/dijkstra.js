fb76628f196a7accddffcb6559163b10c0c04483
Arashdeep Singh <arashdeepsingh301@gmail.com>
2023-02-24 10:43:15 +0530

    pathfinding

---
+++ src/algorithms/dijkstra.js
@@ -0,0 +1,74 @@
+// find shortest path between 2 nodes 
+// works on the basis that the subpath of the shortest path between A and D is also the shortest path between the vertice on subpath and D.
+// this algorithm uses the oppposite approach.
+// it overestimates the distance of each vertex from the starting vertex.
+// then we visit each node and its neighbours to find the shortest subpath between them.
+// this algorithm uses a greedy approach where we try to find the next best solution hopoing the solution is best for the whole problem.
+// first we put the starting node at 0 and restat INF(infinity).
+// then we move to the neighbour vertex and update its path length.
+// if the path length of adjacent vertex is lesser than the new path length dont update it.
+// Perform dijkstras algorithm; return all nodes in the order in which they were visited.
+// Also makes nodes pointback to their previous node, effectively allowing us to compute the shortest path by backtracking from the finish node.
+
+export function dijkstra(grid, startNode, finishNode) {
+    const visitedNodesInOrder = [];
+    startNode.distance = 0;
+    const unvisitedNodes = getAllNodes(grid);
+    while (!!unvisitedNodes.length) {
+      sortNodesByDistance(unvisitedNodes);
+      const closestNode = unvisitedNodes.shift();
+      // If we encounter a wall, we skip it.
+      if (closestNode.isWall) continue;
+      // If the closest node is at a distance of infinity,
+      // we must be trapped and should therefore stop.
+      if (closestNode.distance === Infinity) return visitedNodesInOrder;
+      closestNode.isVisited = true;
+      visitedNodesInOrder.push(closestNode);
+      if (closestNode === finishNode) return visitedNodesInOrder;
+      updateUnvisitedNeighbors(closestNode, grid);
+    }
+  }
+  
+  function sortNodesByDistance(unvisitedNodes) {
+    unvisitedNodes.sort((nodeA, nodeB) => nodeA.distance - nodeB.distance);
+  }
+  
+  function updateUnvisitedNeighbors(node, grid) {
+    const unvisitedNeighbors = getUnvisitedNeighbors(node, grid);
+    for (const neighbor of unvisitedNeighbors) {
+      neighbor.distance = node.distance + 1;
+      neighbor.previousNode = node;
+    }
+  }
+  
+  function getUnvisitedNeighbors(node, grid) {
+    const neighbors = [];
+    const {col, row} = node;
+    if (row > 0) neighbors.push(grid[row - 1][col]);
+    if (row < grid.length - 1) neighbors.push(grid[row + 1][col]);
+    if (col > 0) neighbors.push(grid[row][col - 1]);
+    if (col < grid[0].length - 1) neighbors.push(grid[row][col + 1]);
+    return neighbors.filter(neighbor => !neighbor.isVisited);
+  }
+  
+  function getAllNodes(grid) {
+    const nodes = [];
+    for (const row of grid) {
+      for (const node of row) {
+        nodes.push(node);
+      }
+    }
+    return nodes;
+  }
+  
+  // Backtracks from the finishNode to find the shortest path.
+  // Only works when called *after* the dijkstra method above.
+  export function getNodesInShortestPathOrder(finishNode) {
+    const nodesInShortestPathOrder = [];
+    let currentNode = finishNode;
+    while (currentNode !== null) {
+      nodesInShortestPathOrder.unshift(currentNode);
+      currentNode = currentNode.previousNode;
+    }
+    return nodesInShortestPathOrder;
+  }
